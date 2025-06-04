19b5691ed6024dd543b74f8d5f3845c13120f94f
DominikGruszka <dominikgruszka@poczta.onet.pl>
2025-06-04 18:21:29 +0200

    Delete Models directory

--- Models/Order.cs
+++
@@ -1,9 +0,0 @@
-namespace EcommerceApi.Models;
-
-public class Order
-{
-    public int Id { get; set; }
-    public DateTime OrderDate { get; set; } = DateTime.UtcNow;
-
-    public ICollection<Product> Products { get; set; } = new List<Product>();
-}
